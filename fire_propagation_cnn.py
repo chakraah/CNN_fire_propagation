9933b60fc53acf5e5df1d0edb070e23c3e6a41a5
chakraa <hamza.chakraa@gmail.com>
2025-02-18 15:34:21 +0100

    update

--- fire_propagation_cnn.py
+++ fire_propagation_cnn.py
@@ -10,48 +10,29 @@ from torch.utils.data import Dataset
 class FirePropagationCNN(nn.Module):
     def __init__(self):
         super(FirePropagationCNN, self).__init__()
-        self.conv1 = nn.Conv2d(2, 16, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
         
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(32)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout = nn.Dropout(0.5)
         
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.bn4 = nn.BatchNorm2d(128)
-        
-        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.bn5 = nn.BatchNorm2d(256)
+        self.fc1 = nn.Linear(128 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, num_classes)
 
-        self.conv6 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-        self.bn6 = nn.BatchNorm2d(128)
-
-        self.conv7 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-        self.bn7 = nn.BatchNorm2d(64)
-
-        self.conv8 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
-        self.bn8 = nn.BatchNorm2d(32)
-
-        self.conv9 = nn.Conv2d(32, 16, kernel_size=3, padding=1)
-        self.bn9 = nn.BatchNorm2d(16)
-        
-        self.conv10 = nn.Conv2d(16, 2, kernel_size=3, padding=1)
-        
-        self.sigmoid = nn.Sigmoid()
-        
     def forward(self, x):
         x = torch.relu(self.bn1(self.conv1(x)))
-        x = torch.relu(self.bn2(self.conv2(x)))
-        x = torch.relu(self.bn3(self.conv3(x)))
-        x = torch.relu(self.bn4(self.conv4(x)))
-        x = torch.relu(self.bn5(self.conv5(x)))
-        x = torch.relu(self.bn6(self.conv6(x)))
-        x = torch.relu(self.bn7(self.conv7(x)))
-        x = torch.relu(self.bn8(self.conv8(x)))
-        x = torch.relu(self.bn9(self.conv9(x)))
-        x = self.sigmoid(self.conv10(x))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        
+        x = torch.flatten(x, start_dim=1)
+        x = torch.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        
         return x
 
  
