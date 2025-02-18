f18d267a1075ad60db0fe0f6c129ede1193056e2
chakraa <hamza.chakraa@gmail.com>
2025-02-18 15:43:33 +0100

    update

--- fire_propagation_cnn.py
+++ fire_propagation_cnn.py
@@ -4,37 +4,41 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 
+from fire_propagation_model.fire_propagation_model import WildfireSimulation
 
 # CNN Model for Predicting Fire Spread
-    
 class FirePropagationCNN(nn.Module):
-    def __init__(self, num_classes=2):
+    def __init__(self):
         super(FirePropagationCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=2, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
         
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.dropout = nn.Dropout(0.5)
+        self.conv1 = nn.Conv2d(2, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
         
-        self.fc1 = nn.Linear(128 * 4 * 4, 256)
-        self.fc2 = nn.Linear(256, num_classes)
-
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        
+        self.conv4 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        
+        self.conv5 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
+        self.bn5 = nn.BatchNorm2d(64)
+        
+        self.conv6 = nn.Conv2d(64, 2, kernel_size=3, padding=1)
+        
+        self.dropout = nn.Dropout(0.3)  # Helps prevent overfitting
+        self.sigmoid = nn.Sigmoid()
+    
     def forward(self, x):
-        x = torch.relu(self.bn1(self.conv1(x)))
-        x = self.pool(torch.relu(self.bn2(self.conv2(x))))
-        x = self.pool(torch.relu(self.bn3(self.conv3(x))))
-        
-        x = torch.flatten(x, start_dim=1)
-        x = torch.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.dropout(F.relu(self.bn3(self.conv3(x))))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = self.sigmoid(self.conv6(x))
         return x
-
  
 # Dataset for training
 class FirePropagationDataset(Dataset):
