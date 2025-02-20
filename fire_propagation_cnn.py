27df7d4d1df93c77b86e4b6bfa70bee0a6080993
chakraa <hamza.chakraa@gmail.com>
2025-02-20 15:09:19 +0100

    update

--- fire_propagation_cnn.py
+++ fire_propagation_cnn.py
@@ -27,7 +27,7 @@ class FirePropagationCNN(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        x = self.sigmoid(self.conv3(x))
+        x = self.sigmoid(self.conv4(x))
         return x
  
 # Dataset for training
