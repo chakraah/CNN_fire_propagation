476cbffdd1d7c563262728452a7ac28665440296
chakraa <hamza.chakraa@gmail.com>
2025-02-21 10:04:28 +0100

    update

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
         NUM_EPOCHS = 200
 
         # Initialize loss function, and optimizer
-        loss_function = nn.BCELoss()
+        loss_function = nn.BCEWithLogitsLoss()
         optimizer = optim.Adam(model.parameters(), LEARNING_RATE)
 
         for scenario_idx in range(NUM_SCENARIOS):
